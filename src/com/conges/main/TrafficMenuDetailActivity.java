82c1d33632193788277e4647e51df73c26823067
HollyDAI <mayholly53@gmail.com>
2016-05-18 22:11:33 +0800

    20160518

--- src/com/conges/main/TrafficMenuDetailActivity.java
+++ src/com/conges/main/TrafficMenuDetailActivity.java
@@ -7,6 +7,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ public class TrafficMenuDetailActivity extends Activity {
 	TextView tv_tableLine;
 	EditText tableLineText;
 	ImageButton lowButton,middleButton,highButton;
+	TextView lowTv,middleTv,highTv;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +62,15 @@ public class TrafficMenuDetailActivity extends Activity {
 	private void setTypeDetail(int type) {
 		tv_tableLine = (TextView) findViewById(R.id.traffictypedetail_tv_tableLine);
 		
-		LinearLayout btLinear = (LinearLayout) findViewById(R.id.traffictypedetail_imabt_linearlayout);
+		LinearLayout btLinear = (LinearLayout) findViewById(R.id.traffictypedetail_linearlayout_imabts);
 		lowButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_low);
 		middleButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_middle);
 		highButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_high);
+		
+		LinearLayout tvLinear = (LinearLayout) findViewById(R.id.traffictypedetail_linearlayout_tvs);
+		lowTv = (TextView) findViewById(R.id.traffictypedetail_tv_low);
+		middleTv = (TextView) findViewById(R.id.traffictypedetail_tv_middle);
+		highTv = (TextView) findViewById(R.id.traffictypedetail_tv_high);
 		
 		String str_tableLine = null;
 		float weightSum = 0.0f;
@@ -73,23 +80,41 @@ public class TrafficMenuDetailActivity extends Activity {
 			weightSum = 3.0f;
 			btLinear.setWeightSum(weightSum);
 			lowButton.setBackgroundResource(R.drawable.mark_jam_middle);
+			lowButton.setScaleType(ScaleType.CENTER_INSIDE);
 			middleButton.setBackgroundResource(R.drawable.mark_jam_serious);
 			highButton.setBackgroundResource(R.drawable.mark_jam_fixed);
+			lowTv.setText("中等");
+			middleTv.setText("严重");
+			highTv.setText("不动");
 			break;
 		case 2:
 			str_tableLine = "事故";
 			weightSum = 2.0f;
 			btLinear.setWeightSum(weightSum);
+			lowButton.setBackgroundResource(R.drawable.mark_accident_low);
+			highButton.setBackgroundResource(R.drawable.mark_accident_high);
+			lowTv.setText("轻微");
+			highTv.setText("严重");
 			break;
 		case 3:
 			str_tableLine = "危险";
 			weightSum = 3.0f;
 			btLinear.setWeightSum(weightSum);
+			lowButton.setBackgroundResource(R.drawable.mark_warning_road);
+			middleButton.setBackgroundResource(R.drawable.mark_warning_roadside);
+			highButton.setBackgroundResource(R.drawable.mark_warning_weather);
+			lowTv.setText("路上");
+			middleTv.setText("路边");
+			highTv.setText("天气");
 			break;
 		case 4:
 			str_tableLine = "摄像头";
 			weightSum = 2.0f;
 			btLinear.setWeightSum(weightSum);
+			lowButton.setBackgroundResource(R.drawable.mark_camera_speed);
+			highButton.setBackgroundResource(R.drawable.mark_camera_redlight);
+			lowTv.setText("速度");
+			highTv.setText("红灯");
 			break;
 		case 5:
 			str_tableLine = "地图聊天";
