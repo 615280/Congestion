aac50d0f6f3b54910cc979bc8cd365f8fd37c518
HollyDAI <mayholly53@gmail.com>
2016-05-18 15:45:51 +0800

    20160518

--- src/com/conges/main/TrafficMenuDetailActivity.java
+++ src/com/conges/main/TrafficMenuDetailActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TrafficMenuDetailActivity extends Activity {
 	TextView tv_tableLine;
 	EditText tableLineText;
+	ImageButton lowButton,middleButton,highButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,36 +46,68 @@ public class TrafficMenuDetailActivity extends Activity {
 				// TODO Auto-generated method stub
 				//make connection with Server
 				Toast.makeText(TrafficMenuDetailActivity.this, "发布上传", Toast.LENGTH_SHORT).show();
+				boolean result = true;
+				if(result){
+					Toast.makeText(TrafficMenuDetailActivity.this, "上传成功", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(TrafficMenuDetailActivity.this, "发布失败，请稍候再试！", Toast.LENGTH_LONG).show();
+				}
+				finish();
 			}
 		});
 	}
 
 	private void setTypeDetail(int type) {
 		tv_tableLine = (TextView) findViewById(R.id.traffictypedetail_tv_tableLine);
+		
+		LinearLayout btLinear = (LinearLayout) findViewById(R.id.traffictypedetail_imabt_linearlayout);
+		lowButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_low);
+		middleButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_middle);
+		highButton = (ImageButton) findViewById(R.id.traffictypedetail_bt_high);
+		
 		String str_tableLine = null;
+		float weightSum = 0.0f;
 		switch (type) {
 		case 1:
 			str_tableLine = "交通堵塞";
+			weightSum = 3.0f;
+			btLinear.setWeightSum(weightSum);
+			lowButton.setBackgroundResource(R.drawable.mark_jam_middle);
+			middleButton.setBackgroundResource(R.drawable.mark_jam_serious);
+			highButton.setBackgroundResource(R.drawable.mark_jam_fixed);
 			break;
 		case 2:
 			str_tableLine = "事故";
+			weightSum = 2.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		case 3:
 			str_tableLine = "危险";
+			weightSum = 3.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		case 4:
 			str_tableLine = "摄像头";
+			weightSum = 2.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		case 5:
 			str_tableLine = "地图聊天";
+			weightSum = 0.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		case 6:
 			str_tableLine = "地图问题";
+			weightSum = 0.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		default:
 			str_tableLine = "程序出现问题";
+			weightSum = 0.0f;
+			btLinear.setWeightSum(weightSum);
 			break;
 		}
+		
 		tv_tableLine.setText(str_tableLine);
 	}
 }
